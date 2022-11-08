ac1fed5cbaf456f1086d9854b0dcc82dd0dd055c
CarolinaCedro <yotechgirl@gmail.com>
2022-11-08 14:36:20 -0300

    feat: first test completed

---
+++ src/main/java/io/github/CarolinaCedro/libraryapi/api/dto/BookDto.java
@@ -0,0 +1,16 @@
+package io.github.CarolinaCedro.libraryapi.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+}
